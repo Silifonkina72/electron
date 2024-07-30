16356e82c628f5a0c62f9c08988b597d1daf64f6
Olga <stihina.olga.92@mail.ru>
2024-07-30 17:14:48 +0500

    release 1

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,30 @@
+import {
+  ActionButtonsRow,
+  Content,
+  DraggableTopBar,
+  FloatingNoteTitle,
+  MarkdownEditor,
+  NotePreviewList,
+  RootLayout,
+  Sidebar
+} from '@/components'
+
+const App = () => {
+  return (
+    <>
+      <DraggableTopBar />
+      <RootLayout>
+        <Sidebar className="p-2 ">
+          <ActionButtonsRow className="flex justify-between mt-1" />
+          <NotePreviewList className="mt-3 spae-y-1" />
+        </Sidebar>
+        <Content className="border-l bg-zinc-900/50 border-l-white/20">
+          <FloatingNoteTitle className='pt-2' />
+          <MarkdownEditor />
+        </Content>
+      </RootLayout>
+    </>
+  )
+}
+
+export default App
